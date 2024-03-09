6bb3097f5ad7aceb5cde16532314532eb5922064
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-09 19:33:56 +0300

    add assets, property images and navbar

---
+++ app/properties/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h1>Properties</h1>
+      <Link href="/">Go back home</Link>
+    </div>
+  );
+};
+export default PropertiesPage;
