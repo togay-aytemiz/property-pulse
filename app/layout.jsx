afa54e2bd2b056834ff43d4087cb316c29b1d3ea
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-09 19:04:21 +0300

    Created layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find the perfect property",
+  description: "Find your dream property",
+  keywords: "rental, find rentals, find properties, properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+export default MainLayout;
