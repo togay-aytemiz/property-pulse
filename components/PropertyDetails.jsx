80222fc55fefa59e66671cac025129735fb08896
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-12 21:18:51 +0300

    add property details and spinner, intall next auth and set google provider

---
+++ components/PropertyDetails.jsx
@@ -0,0 +1,102 @@
+import {
+  FaBed,
+  FaBath,
+  FaRuler,
+  FaTimes,
+  FaCheck,
+  FaMap,
+} from "react-icons/fa";
+
+const PropertyDetails = ({ property }) => {
+  return (
+    <main>
+      <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+        <div className="text-gray-500 mb-4">{property.type}</div>
+        <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
+        <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+          <FaMap className="mt-1 text-orange-700 mr-2" />
+          <p className="text-orange-700">
+            {property.location.street} {property.location.city},{" "}
+            {property.location.state} {property.location.zipcode}
+          </p>
+        </div>
+
+        <h3 className="text-lg font-bold my-6 bg-gray-800 text-white p-2">
+          Rates & Options
+        </h3>
+        <div className="flex flex-col md:flex-row justify-around">
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Nightly</div>
+            <div className="text-2xl font-bold">
+              {property.rates.nightly ? (
+                <div className="text-2xl font-bold text-blue-500">
+                  ${property.rates.nightly.toLocaleString()}
+                </div>
+              ) : (
+                <FaTimes className="fa fa-xmark text-red-700" />
+              )}
+            </div>
+          </div>
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Weekly</div>
+            {property.rates.weekly ? (
+              <div className="text-2xl font-bold text-blue-500">
+                ${property.rates.weekly.toLocaleString()}
+              </div>
+            ) : (
+              <FaTimes className="fa fa-xmark text-red-700" />
+            )}
+          </div>
+          <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Monthly</div>
+            {property.rates.monthly ? (
+              <div className="text-2xl font-bold text-blue-500">
+                ${property.rates.monthly.toLocaleString()}
+              </div>
+            ) : (
+              <FaTimes className="fa fa-xmark text-red-700" />
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <h3 className="text-lg font-bold mb-6">Description & Details</h3>
+        <div className="flex justify-start gap-4 text-blue-500 mb-4 text-xl space-x-9">
+          <p>
+            <FaBed className="inline mr-1" /> {property.beds}{" "}
+            <span className="hidden sm:inline">Beds</span>
+          </p>
+          <p>
+            <FaBath className="inline mr-1" /> {property.baths}{" "}
+            <span className="hidden sm:inline">Baths</span>
+          </p>
+          <p>
+            <FaRuler className="inline mr-1" />
+            {property.square_feet}{" "}
+            <span className="hidden sm:inline">sqft</span>
+          </p>
+        </div>
+        <p className="text-gray-500 mb-4">{property.description}</p>
+      </div>
+
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <h3 className="text-lg font-bold mb-6">Amenities</h3>
+
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-1">
+          {property.amenities.map((item, index) => {
+            return (
+              <li key={index}>
+                <FaCheck className=" text-green-600 inline mr-1 " /> {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <div id="map"></div>
+      </div>
+    </main>
+  );
+};
+export default PropertyDetails;
